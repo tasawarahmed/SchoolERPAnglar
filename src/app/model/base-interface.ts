105ca1b1a3f60b13e04ef5adf67510f15e3946e5
tasawarahmed <49127897+tasawarahmed@users.noreply.github.com>
2022-08-15 09:27:10 +0500

    Started Working on Model

---
+++ src/app/model/base-interface.ts
@@ -0,0 +1,5 @@
+export interface BaseInterface {
+  id: number,
+  name: string,
+  description: string,
+}
---
+++ src/app/model/student-interface.ts
@@ -0,0 +1,7 @@
+import { BaseInterface } from "./base-interface";
+
+export class Student implements BaseInterface{
+  id: number = 0;
+  name: string = '';
+  description: string = '';
+}
